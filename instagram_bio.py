ee422fe4260b00cd6f2c4e34b859021b05f430f9
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-06-04 20:32:54 +1000

    Fixed deprecated warnings, added counter break

--- instagram_bio.py
+++ instagram_bio.py
@@ -1,5 +1,6 @@
 # Creation Date: 01/06/2022
 
+
 import os
 import pytz  
 import calendar
@@ -8,6 +9,7 @@ from random import randint
 from datetime import datetime
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -29,11 +31,11 @@ def login(browser):
     username, password = get_secrets()
     browser.get('https://www.instagram.com/')
     sleep(randint(10,20))
-    username_input = browser.find_element_by_css_selector("input[name='username']")
-    password_input = browser.find_element_by_css_selector("input[name='password']")
+    username_input = browser.find_element(By.CSS_SELECTOR, "input[name='username']")
+    password_input = browser.find_element(By.CSS_SELECTOR, "input[name='password']")
     username_input.send_keys(username)
     password_input.send_keys(password)
-    login_button = browser.find_element_by_xpath("//button[@type='submit']")
+    login_button = browser.find_element(By.XPATH, "//button[@type='submit']")
     login_button.click()
     sleep(randint(10,20))
 
@@ -46,28 +48,38 @@ def build_text():
     return f"Feels like {hour} on a {day} to me..."
 
 
-def update_bio(browser):
+def get_current(browser):
     ''' Purpose: Updated Instagram biography as specified. '''
     browser.get('https://www.instagram.com/accounts/edit/')
     sleep(randint(10,20))
-    biography_input = browser.find_element_by_css_selector("textarea[id='pepBio']")
-    current_text = biography_input.get_attribute("value")
-    while True:
-        new_text = build_text()
-        if current_text != new_text:
-            biography_input.clear()
-            biography_input.send_keys(new_text)
-            update_button = browser.find_element_by_xpath("//*[contains(text(), 'Submit')]")
-            update_button.click()
-            current_text = new_text
-        sleep(randint(30,45))
+    biography_input = browser.find_element(By.CSS_SELECTOR, "textarea[id='pepBio']")
+    return biography_input.get_attribute("value")
+    
+def update_text(browser, current_text: str):
+    new_text = build_text()
+    if current_text != new_text:
+        biography_input = browser.find_element(By.CSS_SELECTOR, "textarea[id='pepBio']")
+        biography_input.clear()
+        biography_input.send_keys(new_text)
+        update_button = browser.find_element(By.XPATH ,"//*[contains(text(), 'Submit')]")
+        update_button.click()
+    sleep(randint(30,45))
+    return new_text
 
 
 if __name__ == "__main__":
+    fail = 0
     while True:
         try:
             browser = setup()
             login(browser)
-            update_bio(browser)
+            current_text = get_current(browser)
+            while True:
+                current_text = update_text(browser, current_text)
+                fail = 0
         except Exception:
+            print(1)
+            fail += 1
+            if fail >= 10:
+                break     
             sleep(randint(1200,1300))
