ed8a51a4dade673591b449b74d0d050dbfc48648
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-06-04 23:32:10 +1000

    Added docker, fixed stuff

--- instagram_bio.py
+++ instagram_bio.py
@@ -2,7 +2,8 @@
 
 
 import os
-import pytz  
+import sys
+import pytz
 import calendar
 from time import sleep
 from random import randint
@@ -12,11 +13,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+# See selenium production: http://localhost:4444/ui#/sessions
 
-def setup():
+def setup(method:str = "local"):
     ''' Returns: Browser session. '''
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    browser.implicitly_wait(1)
+    if method == "production":
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-ssl-errors=yes')
+        options.add_argument('--ignore-certificate-errors')
+        browser = webdriver.Remote(
+            command_executor='http://selenium:4444/wd/hub',
+            options=options)
+    else:
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.implicitly_wait(5)
     return browser
 
 
@@ -30,14 +40,16 @@ def login(browser):
     ''' Purpose: Logs into Instagram via Selenium. '''
     username, password = get_secrets()
     browser.get('https://www.instagram.com/')
-    sleep(randint(10,20))
-    username_input = browser.find_element(By.CSS_SELECTOR, "input[name='username']")
-    password_input = browser.find_element(By.CSS_SELECTOR, "input[name='password']")
+    sleep(randint(10, 20))
+    username_input = browser.find_element(
+        By.CSS_SELECTOR, "input[name='username']")
+    password_input = browser.find_element(
+        By.CSS_SELECTOR, "input[name='password']")
     username_input.send_keys(username)
     password_input.send_keys(password)
     login_button = browser.find_element(By.XPATH, "//button[@type='submit']")
     login_button.click()
-    sleep(randint(10,20))
+    sleep(randint(10, 20))
 
 
 def build_text():
@@ -51,34 +63,46 @@ def build_text():
 def get_current(browser):
     ''' Purpose: Updated Instagram biography as specified. '''
     browser.get('https://www.instagram.com/accounts/edit/')
-    sleep(randint(10,20))
-    biography_input = browser.find_element(By.CSS_SELECTOR, "textarea[id='pepBio']")
+    sleep(randint(10, 20))
+    biography_input = browser.find_element(
+        By.CSS_SELECTOR, "textarea[id='pepBio']")
     return biography_input.get_attribute("value")
-    
+
+
 def update_text(browser, current_text: str):
     new_text = build_text()
     if current_text != new_text:
-        biography_input = browser.find_element(By.CSS_SELECTOR, "textarea[id='pepBio']")
+        biography_input = browser.find_element(
+            By.CSS_SELECTOR, "textarea[id='pepBio']")
         biography_input.clear()
         biography_input.send_keys(new_text)
-        update_button = browser.find_element(By.XPATH ,"//*[contains(text(), 'Submit')]")
+        update_button = browser.find_element(
+            By.XPATH, "//*[contains(text(), 'Submit')]")
         update_button.click()
-    sleep(randint(30,45))
+        print(f"Updated text to {new_text} at {datetime.now()}")
+    sleep(randint(30, 45))
     return new_text
 
 
 if __name__ == "__main__":
     fail = 0
-    while True:
+    while fail <= 10:
+        environment = sys.argv[1] if len(sys.argv) >= 2 else "local"
+        print(f"Running as: {environment}")
+        browser = setup(environment)
         try:
-            browser = setup()
             login(browser)
+            print("Login success!")
             current_text = get_current(browser)
+            print(f"Current text: {current_text}")
             while True:
                 current_text = update_text(browser, current_text)
                 fail = 0
-        except Exception:
+        except Exception as e:
+            print(f"Failed: \n{e}")
+            browser.quit()
             fail += 1
-            if fail >= 10:
-                break     
-            sleep(randint(1200,1300))
+            sleep(randint(1200, 1300))
+        except KeyboardInterrupt:
+            browser.quit()
+            break
